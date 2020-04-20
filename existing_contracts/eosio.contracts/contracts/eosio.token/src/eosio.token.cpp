ec2e3d11e91b8c55a884a10068c5b4105c31bc83
Weicheng Xue <weich97@vt.edu>
2020-04-20 03:39:19 -0400

    Fix a sign

--- existing_contracts/eosio.contracts/contracts/eosio.token/src/eosio.token.cpp
+++ existing_contracts/eosio.contracts/contracts/eosio.token/src/eosio.token.cpp
@@ -133,7 +133,7 @@ void token::checkbid( const name& owner, const asset& bid_price )
    auto sym = bid_price.symbol.code();
    accounts accountstable( get_self(), owner.value );
    const auto& ac = accountstable.get( sym.raw() );
-   check( ac.balance.amount > bid_price.amount, "Your balance is too low to bid." );
+   check( ac.balance.amount >= bid_price.amount, "Your balance is too low to bid." );
 }
 
 void token::open( const name& owner, const symbol& symbol, const name& ram_payer )
