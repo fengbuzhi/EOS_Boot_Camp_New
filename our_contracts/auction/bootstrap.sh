1d58832f34170a937c5e688f31660bae17c0c352
Weicheng Xue <weich97@vt.edu>
2020-04-24 13:25:49 -0400

    Add right for ticket3

--- our_contracts/auction/bootstrap.sh
+++ our_contracts/auction/bootstrap.sh
@@ -14,5 +14,9 @@ cleos set account permission weicheng active '{"threshold": 1,"keys": [{"key": "
 cleos set account permission aanuo active '{"threshold": 1,"keys": [{"key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV","weight": 1}],"accounts": [{"permission":{"actor":"auction","permission":"eosio.code"},"weight":1}]}' owner -p aanuo
 cleos set account permission zejia active '{"threshold": 1,"keys": [{"key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV","weight": 1}],"accounts": [{"permission":{"actor":"auction","permission":"eosio.code"},"weight":1}]}' owner -p zejia
 
+cleos set account permission ticket1 active '{"threshold": 1,"keys": [{"key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV","weight": 1}],"accounts": [{"permission":{"actor":"auction","permission":"eosio.code"},"weight":1}]}' owner -p ticket1
+cleos set account permission ticket2 active '{"threshold": 1,"keys": [{"key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV","weight": 1}],"accounts": [{"permission":{"actor":"auction","permission":"eosio.code"},"weight":1}]}' owner -p ticket2
+cleos set account permission ticket3 active '{"threshold": 1,"keys": [{"key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV","weight": 1}],"accounts": [{"permission":{"actor":"auction","permission":"eosio.code"},"weight":1}]}' owner -p ticket3
+
 #Deploy the contract 
 cleos set contract auction . --abi auction.abi -p auction@active
