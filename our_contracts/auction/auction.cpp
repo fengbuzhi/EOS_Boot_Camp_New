cb02c7c408ce35d17e09d51a8ad28c5b0463b6d4
Weicheng Xue <weich97@vt.edu>
2020-04-24 20:37:43 -0400

    Use new libs

--- our_contracts/auction/auction.cpp
+++ our_contracts/auction/auction.cpp
@@ -1,24 +1,117 @@
-#include <math.h>
-#include<vector>
-
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
 #include <eosio/system.hpp>
-//#include <eosio.token/eosio.token.hpp>
+
 #include <eosio/print.hpp>
 #include <eosio/time.hpp>
-//#include <eosiolib/public_key.hpp>
 
 #include "abieos_numeric.hpp"
 
-#define VToken symbol("VTOKEN", 0)
-#define N 3  //# of winners (equal to the number of vailable football tickets for bidding. It can be changed as needed)
+//#include "/workspace/EOS_Boot_Camp_New/our_contracts/auction/eosio.token/include/eosio.token/eosio.token.hpp"
+//#include "/workspace/EOS_Boot_Camp_New/our_contracts/auction/eosio.token/src/eosio.token.cpp"
+
+#define VTOKEN symbol("VTOKEN", 0)
+#define N 3  //# of winners (equal to the number of vailable football tickets for bidding)
+#define RAMCORE_SYMBOL symbol("RAMCORE", 4)
+#define RAM_SYMBOL symbol("RAM", 0)
 
 using namespace eosio;
 using namespace std;
 
+
 class [[eosio::contract("auction")]] auction : public eosio::contract
 {
+
+
+
+
+ private:
+
+    /****************************************************************************
+     *                                D A T A  T Y P E S
+     ***************************************************************************/
+
+    struct permission_level_weight {
+       eosio::permission_level  permission;
+       uint16_t          weight;
+
+    };
+
+    struct wait_weight {
+       uint32_t           wait_sec;
+       uint16_t           weight;
+
+    };
+
+   struct key_weight {
+      abieos::public_key  key;
+      uint16_t           weight;
+
+   };
+
+    struct authority {
+       uint32_t                              threshold = 0;
+       std::vector<key_weight>               keys;
+       std::vector<permission_level_weight>  tickets;
+       std::vector<wait_weight>              waits;
+
+    };
+
+    /****************************************************************************
+     *                                T A B L E S
+     ***************************************************************************/
+
+    TABLE bid_order {
+      name       ticket_for_bid;
+      name       bider;
+      asset      price_ask{0, VTOKEN};
+      time_point time_stamp;
+
+      uint64_t primary_key() const { return ticket_for_bid.value; }
+    };
+
+
+    typedef eosio::multi_index< "bidorders"_n, bid_order > bid_orders;
+    bid_orders _bid_orders;
+
+
+    /****************************************************************************
+     *                            F U N C T I O N S
+     ***************************************************************************/
+
+    //time_point current_time_point() {
+    //  const static time_point ct{ microseconds{ static_cast<eosio::time_point>( current_time_point() ) } };
+    //  return ct;
+    //}
+
+    void create_bid_order( name  bider,
+                            name  ticket_for_bid,
+                            asset price_ask ) {
+      /*bid_orders _bid_orders( _self, bider.value );*/
+      check( _bid_orders.find( ticket_for_bid.value ) == _bid_orders.end(),
+                    "you are already biding this ticket" );
+
+      /* contract ticket pays for ram */
+      auto order = _bid_orders.emplace( _self, [&]( auto& s ) {
+        s.ticket_for_bid = ticket_for_bid;
+        s.bider           = bider;
+        s.price_ask        = price_ask;
+        //s.time_stamp       = current_time_point();
+      });
+    }
+
+  public:
+    using contract::contract;
+
+    // constructor
+    auction( name receiver, name code, datastream<const char*> ds ):
+                  contract( receiver, code, ds ),
+                  _bid_orders( receiver, receiver.value )  {}
+
+
+
+
+
 
 private:
 
@@ -30,35 +123,11 @@ private:
   
   /***Structured data types***/
 
-  struct permission_level_weight {
-    eosio::permission_level permission;
-    uint16_t                weight;
-  };
-
-  struct wait_weight {
-    uint32_t wait_sec;
-    uint16_t weight;
-  };
-
-  struct key_weight {
-    abieos::public_key  key;
-    uint16_t           weight;
-
-  };
-
-  struct authority {
-    uint32_t                              threshold = 0;
-    std::vector<key_weight>               keys;
-    std::vector<permission_level_weight>  tickets;
-    std::vector<wait_weight>              waits;
-
-  };
 
   /***Tables***/
   struct [[eosio::table]] person
   {
     name user_name;
-    int64_t user_id;
     asset funds;
     asset bid_price;
     //Use auxi_price to sort the table in desceding order
@@ -84,87 +153,18 @@ private:
     uint64_t primary_key()const { return balance.symbol.code().raw(); }
   };
 
-  typedef eosio::multi_index<"accounts"_n,account> accounts;
-
   typedef eosio::multi_index<
     "people"_n, person, 
     indexed_by<"byprice"_n, const_mem_fun<person, uint64_t, &person::get_price>>,
     indexed_by<"byauxi"_n, const_mem_fun<person, uint64_t, &person::get_auxi>>
     > bid_index;
-
-  typedef eosio::multi_index<
-    "tickets"_n, ticket
-    //indexed_by<"byticket"_n, const_mem_fun<person, int64_t, &ticket::get_ticket>>
-    > holder_indx;
-
-  //Aanu Database Declaration
-
-  TABLE carpoolog{
-
-      name username;
-      uint64_t log_id;
-      uint64_t car_space;
-      string loc_desc;
-      string loc_param;
-      string cost_jorn;
-      uint64_t delete_post;
-
-      auto primary_key() const {return log_id;}
-      auto by_username() const {return username.value;}
-  };
-  typedef multi_index <name("carpoolt"), carpoolog> carpool_index;
-
-  TABLE joinride{
-
-      name username;
-      uint64_t jridx;
-      uint64_t carpoolid;
-      uint64_t size_req;
-
-      auto primary_key() const {return jridx;}
-      auto by_username() const {return username.value;}
-  };
-  typedef multi_index <name("joinrt"),joinride> joinride_index;
-
-  /***functions***/
-
-  void updateholder( name user, name ticket_to_buy, uint64_t ticket_price ){
-    //Each person can only buy one ticket
-    holder_indx holder_records(get_self(), get_first_receiver().value);
-    auto iterator = holder_records.find(user.value);
-
-    check( iterator == holder_records.end(), "You already have this ticket. You cannot buy it twice in the bidding stage" );  
-
-    holder_records.emplace(user, [&]( auto& row ) {
-          row.holder = user;
-          row.ticket = ticket_to_buy;
-          row.ticket_price = ticket_price;
-    });
-  }
-
 public:
-
-  //Constructor
-  auction(name s, name code, datastream<const char*> ds): contract(s, code, ds) {}
-
-  //Eraser
-  ACTION eraseme(){
-    //require_auth(get_self());
-    bid_index _users(get_self(), get_first_receiver().value);
-    auto msg_itr = _users.begin();
-
-    while (msg_itr != _users.end()) {
-      msg_itr = _users.erase(msg_itr);
-    }
-  }
-
   //Calculate winners
-
   [[eosio::action]]
   void calwinners( )
   {
 
-    bid_index _bid_records( get_self(), get_first_receiver().value) ;
+    bid_index _bid_records( get_self(), get_code().value) ;
 
     //Sort the list in descending order
     auto pridx = _bid_records.get_index<"byauxi"_n>();
@@ -190,17 +190,17 @@ public:
 
   //Places a bid
   [[eosio::action]]
-  void placebid(name user, int64_t user_id, asset bid_price)
+  void placebid(name user, asset bid_price)
   {
     require_auth(user);
 
     auto sym = bid_price.symbol;
 
-    check( bid_price.symbol == VToken, "Symbol precision mismatch." );
+    check( bid_price.symbol == VTOKEN, "Symbol precision mismatch." );
 
     action(
       permission_level{get_self(),"active"_n},
-      "eosio.token"_n, // For online demo/UI, change this to the contract where the eosio.token is set
+      "eosio.token"_n,
       "checkbid"_n,
       std::make_tuple( user, bid_price )
     ).send();		    
@@ -218,15 +218,14 @@ public:
       return;
     }
 
-    bid_index _bid_records( get_self(), get_first_receiver().value) ;
+    bid_index _bid_records( get_self(), get_code().value) ;
 
     auto iterator = _bid_records.find(user.value);
     if ( iterator == _bid_records.end() )
     {
       _bid_records.emplace(user, [&]( auto& row ) {
 	row.user_name = user;
-	row.user_id = user_id;
-	row.bid_price = bid_price;
+	row.bid_price.amount = bid_price.amount;
         //limit the bid price to be less than 10000
 	row.auxi_price = 10000-bid_price.amount;
       });
@@ -235,25 +234,114 @@ public:
     {
       _bid_records.modify(iterator, user, [&]( auto& row ) {
 	row.user_name = user;
-        row.user_id = user_id;
-	row.bid_price = bid_price;
+	row.bid_price.amount = bid_price.amount;
         //limit the bid price to be less than 10000   
 	row.auxi_price = 10000-bid_price.amount;
       });
     }
+
+    
   }
+
+
+
+
+ ACTION clearticket(   name ticket_to_clear,
+                       std::string new_public_key, 
+                       std::string stage) {
+      require_auth( "vtfootball"_n );
+
+      check( stage == "inseason",
+                    "You can not in football season. You cannot trade your ticket" );
+
+      check( is_account( ticket_to_clear ),
+                    "ticket_for_bid does not exist");
+
+      // find ticket for bid / bider / price
+      auto order = _bid_orders.find( ticket_to_clear.value );
+
+      check( order != _bid_orders.end(),
+                    "could not find ticket for bid" );
+
+      // order->bider sends money (price) to bider
+      print( order->bider, " win the '", order->ticket_for_bid,
+             " for ", order->price_ask, "\n" );
+
+      action(
+         permission_level{ order->bider, "active"_n },
+         "eosio.token"_n,
+         "transfer"_n,
+         std::make_tuple(  order->bider,
+                           "vtfootball"_n,
+                          order->price_ask,
+                          std::string("inseason") )
+      ).send();
+
+      action(
+         permission_level{ order->ticket_for_bid, "active"_n },
+         get_self(),
+         "giveauth"_n,
+         std::make_tuple( order->ticket_for_bid, new_public_key )
+      ).send();
+
+      _bid_orders.erase( order );
+
+
+    }
+
+    ACTION giveauth( const name user, const std::string new_owner_pubkey ) {
+      // permission
+      // parent
+      // auth
+      print("*** giveauth ***\n");
+      std::array<uint8_t, 33> owner_pubkey_char;
+
+      const std::string owner_key_str = new_owner_pubkey.substr(0, 53);
+      print("new key: ", owner_key_str, "\n");
+
+      // create new public key
+      const abieos::public_key owner_pubkey =
+            abieos::string_to_public_key( owner_key_str );
+
+      std::copy( owner_pubkey.data.begin(),
+                 owner_pubkey.data.end(),
+                 owner_pubkey_char.begin() );
+
+      // create new authority
+      const auto owner_auth = authority{ 1,
+        {
+          {/*key_weight*/
+            {abieos::key_type::k1, owner_pubkey_char}, 1
+          }
+        },
+        {/*tickets*/},
+        {/*waits*/}
+      };
+
+      // TODO: Change this from changing the "active" permission, to
+      //       changing the "owner" permission!
+      action(
+         permission_level{ user, "active"_n },
+         "eosio"_n,
+         "updateauth"_n,
+         std::make_tuple( user, "active"_n, "owner"_n, owner_auth)
+      ).send();
+
+    }
+
+
 
   //Print winners
   [[eosio::action]]
-  void printwinners(name user)
+  void printwinners(name ticket_for_bid)
   {
-    require_auth(user);
 
     calwinners();
 
     for(auto i = 0; i != N; i++){
       print_f("{winner %}:{name: %, bid_price: %} | ", i+1, winners[i], winners_price[i]);
     }
+      create_bid_order( winners[0], ticket_for_bid, {winners_price[0], VTOKEN} );
   }
   
   //Print all bid prices
@@ -262,7 +350,7 @@ public:
   {
     require_auth(user);
 
-    bid_index _bid_records( get_self(), get_first_receiver().value) ;
+    bid_index _bid_records( get_self(), get_code().value) ;
 
     //Sort the list in descending order
     auto pridx = _bid_records.get_index<"byauxi"_n>();
@@ -271,92 +359,6 @@ public:
     for(auto& iterator : pridx)
       print_f("{bidder %}:{name: %, bid_price: %} | ", indx++, iterator.user_name, iterator.bid_price.amount);
   }
-
-  [[eosio::action]]
-  void buyticket(name user, name ticketinfo, asset bid_price, std::string new_public_key)
-  {
-
-    //check( issuer == "vtfootball"_n, "Only Virginia Tech can distribute tickets!" );
-    //action(
-    //  permission_level{get_self(), "active"_n};
-    //  get_self(),
-    //  "giveauth"_n,
-    //  std::make_tuple( user, new_public_key )
-    //).send();
-
-    require_auth(user);
-
-    calwinners();
-
-    bid_index _bid_records( get_self(), get_first_receiver().value );
-
-    auto iterator = _bid_records.find(user.value);
-    check( iterator != _bid_records.end(), "You are not a bidder. You cannot buy a ticket" );
-    check( bid_price.amount >= winners_price[N-1], "You are not a winner. You cannot buy a ticket" );
-    check( iterator->bid_price.amount == bid_price.amount, "Use your bid price to pay" );
-
-    //if ( iterator == _bid_records.end() ){
-      updateholder( user, ticketinfo, iterator->bid_price.amount );
-    //}
-    
-    //If using iterator->bid_price here, then not working due to a key right issue. Not sure of why
-    action(
-      permission_level{user, "active"_n},
-      "eosio.token"_n, // For online demo/UI, change this to the contract where the eosio.token is set
-      "transfer"_n,
-      std::make_tuple( user,
-                       "vtfootball"_n,
-                       bid_price,
-                       std::string("Initial bidding payment") )
-    ).send();
-
-    //Change the key of the ticket as it has been traded
-    action(
-      permission_level{ ticketinfo, "active"_n },
-      get_self(),
-      "giveauth"_n,
-      std::make_tuple( ticketinfo, new_public_key ) // Not sure whether it should be ticketinfo or user here.
-    ).send();
-  }
-
-  ACTION giveauth( const name user, const std::string new_owner_pubkey ) {
-    // permission
-    // parent
-    // auth
-    print("*** giveauth ***\n");
-    std::array<uint8_t, 33> owner_pubkey_char;
-
-    const std::string owner_key_str = new_owner_pubkey.substr(0, 53);
-    print("new key: ", owner_key_str, "\n");
-
-    // create new public key
-    const abieos::public_key owner_pubkey =
-          abieos::string_to_public_key( owner_key_str );
-
-    std::copy( owner_pubkey.data.begin(),
-               owner_pubkey.data.end(),
-               owner_pubkey_char.begin() );
-
-    // create new authority
-    const auto owner_auth = authority{ 1,
-      {
-        {/*key_weight*/
-          {abieos::key_type::k1, owner_pubkey_char}, 1
-        }
-      },
-      {/*tickets*/},
-      {/*waits*/}
-    };
-
-    // TODO: Change this from changing the "active" permission, to
-    //       changing the "owner" permission!
-    action(
-       permission_level{ user, "active"_n },
-       "eosio"_n,
-       "updateauth"_n,
-       std::make_tuple( user, "active"_n, "owner"_n, owner_auth)
-    ).send();
-
-  }
-
 };
+
+EOSIO_DISPATCH(auction, (placebid)(printbids)(printwinners)(clearticket)(giveauth) )
